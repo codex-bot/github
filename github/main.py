0991329050b56abbdd9af2ac5f57484c20b50372
Taly Guryn <vitalik7tv@yandex.ru>
2017-05-31 22:10:15 +0300

    💂 add start command

--- github/main.py
+++ github/main.py
@@ -16,7 +16,7 @@ class Github:
 
         self.sdk.register_commands([
             ('github_help', 'help', self.help),
-            # ('github_start', 'start', self.start)
+            ('github_start', 'start', self.start)
         ])
 
         self.sdk.set_routes([
@@ -40,11 +40,11 @@ class Github:
                   "- Оповещения о создании Issues\n\n" \
 
         # TODO get repos list
-        # repositories = list()
-        #
-        # if not repositories:
-        #     message += "В данный момент модуль не активирован.\n\n" \
-        #                "Для настройки модуля, используйте команду /github_start"
+        repositories = list()
+
+        if not repositories:
+            message += "В данный момент модуль не активирован.\n\n" \
+                       "Для настройки модуля, используйте команду /github_start"
         # else:
         #     message += "Подключенные репозитории.\n\n"
         #     for repository in repositories:
@@ -53,6 +53,23 @@ class Github:
         #            "Подключить еще один репозиторий можно с помощью команды /github_start\n\n" \
         #            "Меню модуля: /github_help"
 
+        await self.sdk.send_to_chat(
+            payload["chat"],
+            message
+        )
+
+    async def start(self, payload):
+        self.sdk.log("/help handler fired with payload {}".format(payload))
+
+        message = "Чтобы подключить репозиторий, выполните следующие шаги.\n\n" \
+                  "1) Откройте настройки вашего репозитория.\n\n" \
+                  "2) Зайдите в раздел Webhooks & services и нажмите кнопку Add Webhook.\n\n" \
+                  "3) Вставьте в поле Payload URL следующую ссылку.\n{link}\n\n" \
+                  "4) В поле «Which events would you like to trigger this webhook?» выберите " \
+                  "«Let me select individual events» и отметьте следующие флажки: \n" \
+                  "- Issues\n- Pull request\n- Push\n\n" \
+                  "5) В поле «Content type» выберите тип «application/json».\n\n" \
+                  "6) Нажмите на кнопку «Add Webhook»."
         await self.sdk.send_to_chat(
             payload["chat"],
             message
