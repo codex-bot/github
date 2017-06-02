b4ba325825a20af9af4042037335d21476af7496
N0str <ntpcp@yandex.ru>
2017-06-02 19:31:24 +0300

    Github event

--- github/main.py
+++ github/main.py
@@ -1,6 +1,9 @@
 import logging
 from sdk.codexbot_sdk import CodexBot
 from config import APPLICATION_TOKEN, APPLICATION_NAME, DB, URL, SERVER
+from commands.help import CommandHelp
+from commands.start import CommandStart
+from events.ping import EventPing
 
 
 class Github:
@@ -12,8 +15,8 @@ class Github:
         self.sdk.log("Github module initialized")
 
         self.sdk.register_commands([
-            ('github_help', 'help', self.help),
-            ('github_start', 'start', self.start)
+            ('github_help', 'help', CommandHelp(self.sdk).help),
+            ('github_start', 'start', CommandStart(self.sdk).start)
         ])
 
         self.sdk.set_routes([
@@ -22,59 +25,9 @@ class Github:
 
         self.sdk.start_server()
 
-    #
-    #
-    # HELP
-    # todo: move to the class
-    #
-
-    async def help(self, payload):
-        self.sdk.log("/help handler fired with payload {}".format(payload))
-
-        message = "Модуль для работы с сервисом GitHub.\n\n" \
-                  "- Оповещения о новых Push-событиях\n" \
-                  "- Оповещения о создании Pull-реквестов\n" \
-                  "- Оповещения о создании Issues\n\n" \
-
-        # TODO get repos list
-        repositories = list()
-
-        if not repositories:
-            message += "В данный момент модуль не активирован.\n\n" \
-                       "Для настройки модуля, используйте команду /github_start"
-        # else:
-        #     message += "Подключенные репозитории.\n\n"
-        #     for repository in repositories:
-        #         message += "{}\n".format(repository["name"])
-        #     message += "\nДля отключения репозитория используйте команду /github_stop\n" \
-        #            "Подключить еще один репозиторий можно с помощью команды /github_start\n\n" \
-        #            "Меню модуля: /github_help"
-
-        await self.sdk.send_to_chat(
-            payload["chat"],
-            message
-        )
-
-    async def start(self, payload):
-        self.sdk.log("/start handler fired with payload {}".format(payload))
-
-        message = "Чтобы подключить репозиторий, выполните следующие шаги.\n\n" \
-                  "1) Откройте настройки вашего репозитория.\n\n" \
-                  "2) Зайдите в раздел Webhooks & services и нажмите кнопку Add Webhook.\n\n" \
-                  "3) Вставьте в поле Payload URL следующую ссылку.\n{link}\n\n" \
-                  "4) В поле «Which events would you like to trigger this webhook?» выберите " \
-                  "«Let me select individual events» и отметьте следующие флажки: \n" \
-                  "- Issues\n- Pull request\n- Push\n\n" \
-                  "5) В поле «Content type» выберите тип «application/json».\n\n" \
-                  "6) Нажмите на кнопку «Add Webhook»."
-        await self.sdk.send_to_chat(
-            payload["chat"],
-            message
-        )
-
     @CodexBot.http_response
     async def github_callback_handler(self, result):
-        logging.debug(result)
+        EventPing(self.sdk).process(result['headers'])
         return {'text': '', 'status': 200}
 
 if __name__ == "__main__":
