830816f7c1b42af78421a01de41342134792f60c
N0str <ntpcp@yandex.ru>
2018-06-16 19:36:34 +0300

    Fix github_link behaviour

--- github/commands/link.py
+++ github/commands/link.py
@@ -8,6 +8,8 @@ class CommandLink(CommandBase):
     async def __call__(self, payload):
         self.sdk.log("/github_link handler fired with payload {}".format(payload))
 
+        self.set_bot(payload)
+
         user_token = ChatController(self.sdk).register_chat(payload['chat'])
 
         link = "{}/github/{}".format(URL, user_token)
