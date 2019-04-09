ec8dd30f3c7039db887d66674005ac8f235a6014
Taly <vitalik7tv@yandex.ru>
2019-04-09 09:02:37 +0300

    Update fork.py

--- github/events/fork.py
+++ github/events/fork.py
@@ -40,7 +40,7 @@ class EventFork(EventBase):
 
         await self.send(
             chat['chat'],
-            '<a href=\"{}\">{}</a> forked 🦍 <a href=\"{}\">{}</a>.'.format(
+            '<a href=\"{}\">{}</a> 🍴 <a href=\"{}\">{}</a>'.format(
                 self.sender.html_url,
                 self.sender.login,
                 self.repository.html_url,
